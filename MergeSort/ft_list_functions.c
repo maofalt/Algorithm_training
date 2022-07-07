9c250a993f64ff42b7640bd4f01289f1a5a2c131
maofalt <maofalt@gmail.com>
2022-07-07 19:05:57 +0200

    add end and start corrected. Add new fonction, remove node without testing

--- MergeSort/ft_list_functions.c
+++ MergeSort/ft_list_functions.c
@@ -32,19 +32,19 @@ void	ft_node_insert_end(t_list *list, t_data data)
 	new = ft_node_create(data, XOR(list->head, list->tail));
 	if ((!list->head && !list->tail) || !list)
 		*list = ft_list_create(new, new, 0);
-	else if (list->head == list->tail)
-			*list = ft_list_create(new, list->head, 1);
+	else if ((list->head == list->tail) || list->size == 1)
+		*list = ft_list_create(list->head, new, 1);
 	else
 	{
-		if (!list->head->npx || !list->tail->npx)
+		if (list->size == 2)
 		{
-			list->tail->npx = XOR(new, list->head);
 			list->head->npx = XOR(new, list->tail);
+			list->tail->npx = XOR(new, list->head);
 		}
 		else
 		{
-			list->tail->npx = XOR(new, XOR(list->head->npx, list->tail));
-			list->head->npx = XOR(new, XOR(list->tail->npx, list->head));
+			list->head->npx = XOR(new, XOR(list->head->npx, list->tail));
+			list->tail->npx = XOR(new, XOR(list->tail->npx, list->head));
 		}
 		list->tail = new;
 	}
@@ -66,19 +66,54 @@ void	ft_node_insert_start(t_list *list, t_data data)
 	{
 		if (list->size == 2)
 		{
-			printf("we enter here\n");
 			list->head->npx = XOR(new, list->tail);
 			list->tail->npx = XOR(new, list->head);
 		}
 		else
 		{
-			//error is here. Last is not relink to 1 and probably the same in the other sense
 			list->head->npx = XOR(new, XOR(list->head->npx, list->tail));
 			list->tail->npx = XOR(new, XOR(list->tail->npx, list->head));
 		}
 		list->head = new;
 	}
 	list->size++;
+	ft_list_new_index(list);
+}
+
+//We remove a node from the top
+void	ft_node_remove(t_list *list, t_node *node)
+{
+	if ((!list->head && !list->tail) || !list)
+		return ;
+	else if ((list->head == list->tail) || list->size == 1)
+	{
+		node = list->head;
+		*list = ft_list_create(NULL, NULL, 0);
+		node->npx = NULL;
+	}
+	else if (list->size == 2)
+	{
+		node = list->head;
+		*list = ft_list_create(list->tail, list->tail, 1);
+		list->tail->npx = NULL;
+		node->npx = NULL;
+	}
+	else if (list->size == 3)
+	{
+		node = list->head;
+		*list = ft_list_create(XOR(node->npx, list->tail), list->tail, 2);
+		list->head->npx = NULL;
+		list->tail->npx = NULL;
+		node->npx = NULL;
+	}
+	else
+	{
+		node = list->head;
+		*list = ft_list_create(XOR(node->npx, list->tail), list->tail, list->size - 1);
+		list->head->npx = XOR(XOR(list->head->npx, node),list->tail);
+		list->tail->npx = XOR(XOR(node, list->tail->npx),list->head);
+		node->npx = NULL;
+	}
 	ft_list_new_index(list);
 }
 
@@ -205,7 +240,7 @@ int	main()
 	t_data d,a,t,s;
 	t_node	*head = NULL, *tail = NULL;
 	t_list	list;
-	int		i = 0;
+	
 
 	list = ft_list_create(head, tail, 0);
 	d.nb = 1;
@@ -216,18 +251,19 @@ int	main()
 	t.index = 0;
 	s.nb = 4;
 	s.index = 0;
-	ft_node_insert_start(&list, d);
-	ft_node_insert_start(&list, a);
-	ft_node_insert_start(&list, t);
+	ft_node_insert_end(&list, d);
+	ft_node_insert_end(&list, a);
+	ft_node_insert_end(&list, t);
 	ft_node_insert_start(&list, s);
 	ft_list_print_data(list);
+	//int		i = 0;
 	//list2 = ft_list_create(head, tail, 0);
-	printf("After rotation \n");
-	while (i++ < 4)
-	{
-		printf("Rotation [%d]\n", i);
-		list = ft_list_rotate(list);
-		ft_list_print_data(list);
-	}
+	// printf("After rotation \n");
+	// while (i++ < 4)
+	// {
+	// 	printf("Rotation [%d]\n", i);
+	// 	list = ft_list_rotate(list);
+	// 	ft_list_print_data(list);
+	// }
 	ft_list_free(&list);
 }
